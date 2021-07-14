cbddaf93806bf46bfe29e63a0b3d4bbcb0adb054
smj-edison <stephen.mason.jones@gmail.com>
2021-07-13 22:49:35 -0700

    Create component for clicky things

---
+++ src/lib/organ/Button.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let spriteSrcs;
+    export let spriteIndex = 0;
+    export let x;
+    export let y;
+    export let width;
+    export let height;
+    export let rotation = 0;
+</script>
+
+<image href={spriteSrcs[spriteIndex]} {x} {y} {width} {height} transform="rotate({rotation} {x + width / 2} {y + height / 2})" preserveAspectRatio="none" style="cursor: pointer;" on:click />
