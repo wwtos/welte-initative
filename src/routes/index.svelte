3ceb94186791eedd09855421cada89db3dcfbe63
smj-edison <stephen.mason.jones@gmail.com>
2021-07-14 17:02:48 -0700

    Add skeleton code

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Organ from "$lib/organ/Organ.svelte";
+</script>
+
+<svelte:head>
+	<title>Welcome</title>
+</svelte:head>
+
+<Organ />
