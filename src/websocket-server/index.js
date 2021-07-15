bd63506c6c55931291abab30c635671d32c09c46
smj-edison <stephen.mason.jones@gmail.com>
2021-07-15 15:42:57 -0700

    Add websocket server

---
+++ src/websocket-server/index.js
@@ -0,0 +1,13 @@
+import WebSocket from 'ws';
+
+const wss = new WebSocket.Server({
+    port: 3001
+});
+
+wss.on('connection', ws => {
+    ws.on('message', message => {
+      console.log('received: %s', message);
+    });
+
+    ws.send('something');
+});
