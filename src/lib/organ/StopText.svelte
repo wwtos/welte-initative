9658419dac8abc0013be3d92674a26d8fa51c6a2
smj-edison <stephen.mason.jones@gmail.com>
2021-07-15 08:17:44 -0700

    Remove unnecessary parameter

--- src/lib/organ/StopText.svelte
+++ src/lib/organ/StopText.svelte
@@ -3,13 +3,12 @@
     export let y = 0;
     export let textSize = 12;
     export let text = "";
-    export let textColor = "black";
 
     const lines = text.split("\n");
     const spacing = 1.1;
 </script>
 
-<text {x} y={y - (textSize * spacing * lines.length) / 2} alignment-baseline="middle" fill={textColor} style="--text-size: {textSize}px">
+<text {x} y={y - (textSize * spacing * lines.length) / 2} alignment-baseline="middle" style="--text-size: {textSize}px">
     {#each lines as line}
         <tspan x={x} dy="{textSize * spacing}">
             {line}
