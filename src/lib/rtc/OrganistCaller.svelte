65256a6049fd25c36404efbd9580b8583c8dfec3
smj-edison <stephen.mason.jones@gmail.com>
2021-07-27 20:28:57 -0700

    Add component for calling as organist

---
+++ src/lib/rtc/OrganistCaller.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<div>
+    Call area
+    <video id="remote-video"></video>
+    <button>Start call</button>
+</div>
+
+<style>
+div {
+    border: 1px solid #aaa;
+}
+</style>
