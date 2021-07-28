b26145887639f8b235ddfd0ebbbb553d77deec48
smj-edison <stephen.mason.jones@gmail.com>
2021-07-27 20:29:18 -0700

    Add ws server debugging

--- src/websocket-server/index.js
+++ src/websocket-server/index.js
@@ -10,41 +10,50 @@ let organWebsocket = null;
 
 let uid = 0;
 
+function respond(to, message) {
+    console.log("sent", JSON.stringify(message), "to ", to.uid);
+    to.send(JSON.stringify(message));
+}
+
 wss.on('connection', ws => {
     ws.uid = uid++;
     websockets.push(ws);
 
+    console.log("connection");
+
     ws.on('message', message => {
         const request = JSON.parse(message);
+
+        console.log(request);
 
         if(request.type) {
             switch(request.type) {
                 case "organist-stream-offer":
-                    organWebsocket.send(JSON.stringify({
+                    respond(organWebsocket, {
                         "type": "organist-stream-offer",
                         "from": ws.uid,
                         "body": {
                             "sdp": request.body.sdp
                         }
-                    }));
+                    });
                 break;
                 case "organ-stream-answer":
-                    websockets.find(websocket => websocket.uid === request.body.to).send(JSON.stringify({
+                    respond(websockets.find(websocket => websocket.uid === request.body.to), {
                         "type": "organ-stream-answer",
                         "from": ws.uid,
                         "body": {
                             "sdp": request.body.sdp
                         }
-                    }));
+                    });
                 break;
                 case "ice-candidate":
-                    websockets.find(websocket => websocket.uid === request.body.to).send(JSON.stringify({
+                    respond(websockets.find(websocket => websocket.uid === request.body.to), {
                         "type": "ice-candidate",
                         "from": ws.uid,
                         "body": {
                             "candidate": request.body.candidate
                         }
-                    }));
+                    });
                 break;
                 case "am-organ":
                     organWebsocket = ws;
