72674176ae8ce0b037029071c456b584a458d081
smj-edison <stephen.mason.jones@gmail.com>
2021-07-27 20:29:26 -0700

    Add caller boxes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,13 @@
 <script>
 	import Organ from "$lib/organ/Organ.svelte";
 	import DeviceSelector from "$lib/midi/DeviceSelector.svelte";
+	import OrganistCaller from "$lib/rtc/OrganistCaller.svelte";
 </script>
 
 <svelte:head>
 	<title>Welcome</title>
 </svelte:head>
 
+<OrganistCaller />
 <DeviceSelector listInputs="true" />
 <Organ />
--- src/routes/organ-owner.svelte
+++ src/routes/organ-owner.svelte
@@ -1,13 +1,19 @@
 <script>
+	import { writable } from 'svelte/store';
+
 	import Organ from "$lib/organ/Organ.svelte";
 	import DeviceSelector from "$lib/midi/DeviceSelector.svelte";
 	import LocalAV from "$lib/rtc/LocalAV.svelte";
+	import OrganCaller from "$lib/rtc/OrganCaller.svelte";
+
+	const mediaStreamStore = writable();
 </script>
 
 <svelte:head>
 	<title>Welcome</title>
 </svelte:head>
 
-<LocalAV />
+<OrganCaller {mediaStreamStore} />
+<LocalAV {mediaStreamStore} />
 <DeviceSelector listOutputs="true" />
 <Organ />
