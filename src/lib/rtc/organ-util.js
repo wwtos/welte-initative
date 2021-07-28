a8b6538e35318527445dd7497ecd7fc976a55cf3
smj-edison <stephen.mason.jones@gmail.com>
2021-07-27 19:55:28 -0700

    Switch to object based approach for clients

--- src/lib/rtc/organ-util.js
+++ src/lib/rtc/organ-util.js
@@ -2,38 +2,58 @@ import {createPeerConnection} from "./util.js";
 
 import {sendAnswer, sendIceCandidate} from "../ws/organ-ws.js";
 
-export function getConnectionConfig(state) {
-    return {
-        handleICECandidateEvent(event) {
-            if (event.candidate) {
-                sendIceCandidate({
-                    type: "new-ice-candidate",
-                    target: targetUsername,
-                    candidate: event.candidate
-                });
-            }
-        }
-    };
-}
+const OrganConnection = function(ws, localStream) {
+    this.ws = ws;
+    this.peerConnection = null;
+    this.peerId = -1;
+    this.localStream = localStream;
 
-function handleOrganistOffer(ws, wsMessage, stream) {
-    let localStream;
+    this.handleICECandidateEvent = this.handleICECandidateEvent.bind(this);
+    this.handleRemoteICECandidateEvent = this.handleRemoteICECandidateEvent.bind(this);
 
-    const myPeerConnection = createPeerConnection({});
+    this.handleTrackEvent = this.handleTrackEvent.bind(this);
+    this.handleRemoveTrackEvent = this.handleRemoveTrackEvent.bind(this);
+};
 
-    const desc = new RTCSessionDescription(wsMessage.body.sdp);
+OrganConnection.handleOfferMsg = async function(msg) {
+    this.peerId = msg.from;
 
-    myPeerConnection.setRemoteDescription(desc).then(() => {
-        localStream = stream;
+    this.peerConnection = createPeerConnection(this);
 
-        localStream.getTracks().forEach(track => myPeerConnection.addTrack(track, localStream));
-    }).then(() => {
-        return myPeerConnection.createAnswer();
-    }).then(answer => {
-        return myPeerConnection.setLocalDescription(answer);
-    }).then(() => {
-        sendAnswer(ws, wsMessage.body.sdp, wsMessage.body.to);
-    })
+    const desc = new RTCSessionDescription(msg.body.sdp);
 
+    await this.peerConnection.setRemoteDescription(desc);
+    await this.localStream.getTracks().forEach(track => this.peerConnection.addTrack(track, this.localStream));
 
-}
+    const answer = await this.peerConnection.createAnswer();
+    await this.peerConnection.setLocalDescription(answer);
+
+    sendAnswer(this.ws, this.peerConnection.localDescription, msg.from);
+};
+
+OrganConnection.prototype.handleICECandidateEvent = function(event) {
+    if (event.candidate) {
+        sendIceCandidate(this.ws, event.candidate, this.peerId);
+    }
+};
+
+OrganConnection.prototype.handleRemoteICECandidateEvent = function(msg) {
+    const candidate = new RTCIceCandidate(msg.body.candidate);
+
+    this.peerConnection.addIceCandidate(candidate); // TODO: catch errors
+};
+
+OrganConnection.prototype.handleTrackEvent = function(event) {
+    this.videoElement.srcObject = event.streams[0];
+};
+
+OrganConnection.prototype.handleRemoveTrackEvent = function() {
+    // var stream = document.getElementById("received_video").srcObject;
+    // var trackList = stream.getTracks();
+  
+    // if (trackList.length === 0) {
+    //   closeVideoCall();
+    // }
+};
+
+export { OrganConnection };
--- src/lib/rtc/organist-util.js
+++ src/lib/rtc/organist-util.js
@@ -3,22 +3,26 @@ import {sendIceCandidate} from "../ws/ws.js";
 
 import {createPeerConnection} from "./util.js";
 
-const OrganistConnection = function(ws) {
+const OrganistConnection = function(ws, videoElement) {
     this.ws = ws;
+    this.videoElement = videoElement;
     this.peerConnection = null;
     this.peerId = -1;
 
+    this.handleNegotiationNeededEvent = this.handleNegotiationNeededEvent.bind(this);
+
     this.handleICECandidateEvent = this.handleICECandidateEvent.bind(this);
     this.handleRemoteICECandidateEvent = this.handleRemoteICECandidateEvent.bind(this);
+
+    this.handleTrackEvent = this.handleTrackEvent.bind(this);
+    this.handleRemoveTrackEvent = this.handleRemoveTrackEvent.bind(this);
 };
 
-OrganistConnection.prototype.open = function() {
-    this.peerConnection = createPeerConnection({
-        handleICECandidateEvent: this.handleICECandidateEvent
-    });
+OrganistConnection.prototype.createPeerConnection = function() {
+    this.peerConnection = createPeerConnection(this);
 };
 
-OrganistConnection.prototype.sendOffer = async function() {
+OrganistConnection.prototype.handleNegotiationNeededEvent = async function() {
     const offer = await this.peerConnection.createOffer({
         'mandatory': {
             'OfferToReceiveAudio': true,
@@ -26,9 +30,17 @@ OrganistConnection.prototype.sendOffer = async function() {
         }
     });
     
-    await myPeerConnection.setLocalDescription(offer);
+    await this.peerConnection.setLocalDescription(offer);
 
-    sendOffer(this.ws, myPeerConnection.localDescription);
+    sendOffer(this.ws, this.peerConnection.localDescription);
+};
+
+OrganistConnection.prototype.handleAnswerMsg = async function(msg) {
+    const description = new RTCSessionDescription(msg.body.sdp);
+
+    this.peerId = msg.from;
+
+    await this.peerConnection.setLocalDescription(description);
 };
 
 OrganistConnection.prototype.handleICECandidateEvent = function(event) {
@@ -37,42 +49,23 @@ OrganistConnection.prototype.handleICECandidateEvent = function(event) {
     }
 };
 
-OrganistConnection.prototype.handleRemoteICECandidateEvent(wsMessage) {
-    const candidate = new RTCIceCandidate(wsMessage.body.candidate);
+OrganistConnection.prototype.handleRemoteICECandidateEvent = function(msg) {
+    const candidate = new RTCIceCandidate(msg.body.candidate);
 
-
+    this.peerConnection.addIceCandidate(candidate); // TODO: catch errors
 };
 
-function handleTrackEvent() {
+OrganistConnection.prototype.handleTrackEvent = function(event) {
+    this.videoElement.srcObject = event.streams[0];
+};
 
-}
+OrganistConnection.prototype.handleRemoveTrackEvent = function() {
+    // var stream = document.getElementById("received_video").srcObject;
+    // var trackList = stream.getTracks();
+  
+    // if (trackList.length === 0) {
+    //   closeVideoCall();
+    // }
+};
 
-function handleNegotiationNeededEvent() {
-
-}
-
-function handleRemoveTrackEvent() {
-
-}
-
-function handleICEConnectionStateChangeEvent() {
-
-}
-
-function handleICEGatheringStateChangeEvent() {
-
-}
-
-function handleSignalingStateChangeEvent() {
-
-}
-
-export async function createOffer(ws, connectionConfig) {
-    
-
-
-}
-
-export function handleAnswer(ws, answer) {
-
-}
+export { OrganistConnection };
