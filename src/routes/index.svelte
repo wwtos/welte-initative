1d968abf5b24042bda576dd3b57afa3efba1ab71
smj-edison <stephen.mason.jones@gmail.com>
2021-07-28 23:58:18 -0700

    Manage midi

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,36 @@
 <script>
+	import { writable } from 'svelte/store';
+
 	import Organ from "$lib/organ/Organ.svelte";
 	import DeviceSelector from "$lib/midi/DeviceSelector.svelte";
 	import OrganistCaller from "$lib/rtc/OrganistCaller.svelte";
+
+	const inputMidiDeviceStore = writable();
+	const nextMidiMessage = writable();
+
+	let lastInputMidiDevice;
+
+	function handleMidi(midi) {
+		nextMidiMessage.update(_ => midi);
+	}
+
+	inputMidiDeviceStore.subscribe(inputMidiDevice => {
+		if(inputMidiDevice) {
+			if(lastInputMidiDevice) {
+				lastInputMidiDevice.removeEventListener("midimessage", handleMidi);
+			}
+
+			inputMidiDevice.addEventListener("midimessage", handleMidi);
+
+			lastInputMidiDevice = inputMidiDevice;
+		}
+	});
 </script>
 
 <svelte:head>
 	<title>Welcome</title>
 </svelte:head>
 
-<OrganistCaller />
-<DeviceSelector listInputs="true" />
+<OrganistCaller {nextMidiMessage} />
+<DeviceSelector {inputMidiDeviceStore} listInputs="true" />
 <Organ />
--- src/routes/organ-owner.svelte
+++ src/routes/organ-owner.svelte
@@ -6,14 +6,37 @@
 	import LocalAV from "$lib/rtc/LocalAV.svelte";
 	import OrganCaller from "$lib/rtc/OrganCaller.svelte";
 
+	const outputMidiDeviceStore = writable();
 	const mediaStreamStore = writable();
+	const incomingMidiStore = writable();
+
+	let midiDeviceToSendTo;
+
+	function handleMidi(midi) {
+		if(midiDeviceToSendTo) {
+			midiDeviceToSendTo.send(midi);
+		}
+	}
+
+	incomingMidiStore.subscribe(midi => {
+		if(midi) {
+			handleMidi(new Uint8Array(midi));
+		}
+	});
+
+	outputMidiDeviceStore.subscribe(outputMidiDevice => {
+		if(outputMidiDevice) {
+			midiDeviceToSendTo = outputMidiDevice;
+			midiDeviceToSendTo.open();
+		}
+	});
 </script>
 
 <svelte:head>
 	<title>Welcome</title>
 </svelte:head>
 
-<OrganCaller {mediaStreamStore} />
+<OrganCaller {mediaStreamStore} {incomingMidiStore} />
 <LocalAV {mediaStreamStore} />
-<DeviceSelector listOutputs="true" />
+<DeviceSelector {outputMidiDeviceStore} listOutputs="true" />
 <Organ />
